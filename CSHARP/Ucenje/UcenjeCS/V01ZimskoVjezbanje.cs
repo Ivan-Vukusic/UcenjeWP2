c5b6471a6d91b8da8f8897013fc717b2c5d281ef
Ivan Vukušić <ivukusic27@gmail.com>
2023-12-24 11:13:34 +0100

    ...

--- CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -10,47 +10,28 @@ namespace UcenjeCS
 
             //Napisati program koji ispisuje sve brojeve od 1 do 100
 
-            //for (int i = 0; i <= 100; i++) 
-            //{
-            //    Console.WriteLine(i+1);
-            //}
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
 
 
             //Napisati program koji ispisuje sve brojeve od 100 do 1
 
-            //for (int i = 100; i > 0; i--)
-            //{
-            //    Console.WriteLine(i);
-            //}
-
-
-            //Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
-
-            //for (int i = 1; i <= 100; i++)
-            //{
-            //    if(i % 7 == 0) 
-            //    { 
-            //        Console.WriteLine(i); 
-            //    }                                           
-
-            //}
-
-
-            //Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa
-            
-            for (int i = 0; i > 100; i++) 
+            for (int i = 100; i > 0; i--)
             {
-                if(i > 100) 
-                {
-                    break;
-                }
                 Console.WriteLine(i);
             }
 
 
+            //Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
 
-
-
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
-    }
-}
+}   }
