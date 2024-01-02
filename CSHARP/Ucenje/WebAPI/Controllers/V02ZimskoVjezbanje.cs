33f3c98ce1e69d03ac06d0739af04e8a8ae9393e
Ivan Vukušić <ivukusic27@gmail.com>
2024-01-02 18:16:27 +0100

    V02 vježbe

--- CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -6,6 +6,7 @@ namespace WebAPI.Controllers
     [Route("V02")]
     public class V02ZimskoVjezbanje : ControllerBase
     {
+        //Ruta ne prima niti jedan parametar i vraća zbroj prvih 100 brojeva
         [HttpGet]
         [Route("Vjezba1")]
         public int Vj1()
@@ -14,33 +15,34 @@ namespace WebAPI.Controllers
         }
 
 
-
+        //Ruta ne prima niti jedan parametar i vraća niz s svim parnim brojevima od 1 do 57
         [HttpGet]
         [Route("Vjezba2")]
 
-        public int[] Vj2() 
-        {            
+        public int[] Vj2()
+        {
             int[] niz = new int[28];
             int n = 0;
-            
-            for(int i = 1; i < 57; i++) 
+
+            for (int i = 1; i < 57; i++)
             {
-                if(i % 2 == 0) 
+                if (i % 2 == 0)
                 {
                     niz[n++] = i;
-                }                
-            }   
+                }
+            }
             return niz;
         }
 
 
+        //Ruta ne prima niti jedan parametar i vraća zbroj svih parnih brojeva od 2 do 18
         [HttpGet]
         [Route("Vjezba3")]
 
-        public int Vj3() 
+        public int Vj3()
         {
             int suma = 0;
-            for(int i = 2;  i <= 18; i++) 
+            for (int i = 2; i <= 18; i++)
             {
                 if (i % 2 == 0)
                 {
@@ -52,19 +54,155 @@ namespace WebAPI.Controllers
         }
 
 
+        //Ruta prima jedan parametar koji je cijeli broj i vraća zbroj svih brojeva od 1 do primljenog broja
         [HttpGet]
         [Route("Vjezba4")]
 
-        public int Vj4(int broj) 
+        public int Vj4(int broj)
         {
             int suma = 0;
 
-            for(int i = 0; i <= broj; i++) 
+            for (int i = 0; i <= broj; i++)
             {
                 suma += i;
             }
             return suma;
         }
 
+
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća niz sa svim parnim brojevima između primljenih brojeva
+        [HttpGet]
+        [Route("Vjezba5")]
+        public int[] Vj5(int x, int y)
+        {
+
+            int a = 0;
+            int b = 0;
+
+            if (x < y)
+            {
+                a = x;
+                b = y;
+            }
+            else
+            {
+                a = y;
+                b = x;
+            }
+
+            int[] niz = new int[b / 2];
+            int n = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    niz[n++] = i;
+                }
+            }
+            return niz;
+        }
+
+
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća niz s svim neparnim brojevima između primljenih brojeva
+        [HttpGet]
+        [Route("Vjezba6")]
+        public int[] Vj6(int x, int y)
+        {
+            int a = 0;
+            int b = 0;
+
+            if (x < y)
+            {
+                a = x;
+                b = y;
+            }
+            else
+            {
+                a = y;
+                b = x;
+            }
+
+            int[] niz = new int[b / 2];
+            int n = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    niz[n++] = i;
+                }
+            }
+            return niz;
+        }
+
+
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva
+        [HttpGet]
+        [Route("Vjezba7")]
+        public int Vj7(int x, int y)
+        {
+            int suma = 0;
+            int a = 0;
+            int b = 0;
+
+            if (x < y)
+            {
+                a = x;
+                b = y;
+            }
+            else
+            {
+                a = y;
+                b = x;
+            }
+
+            for (int i = a; i <= b; i++)
+            {
+                suma += i;
+            }
+
+            return suma;
+        }
+
+
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva koji su cjelobrojno djeljivi s 3
+        [HttpGet]
+        [Route("Vjezba8")]
+        public int Vj8(int x, int y)
+        {
+            int suma = 0;
+            int a = 0;
+            int b = 0;
+
+            if (x < y)
+            {
+                a = x;
+                b = y;
+            }
+            else
+            {
+                a = y;
+                b = x;
+            }
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    suma += i;
+                }
+            }
+            return suma;
+        }
+
+
+        //Ruta prima dva parametra koji su cijeli brojevi i vraća zbroj svih brojeva između primljenih brojeva koji su cjelobrojno djeljivi s 3 i 5
+        [HttpGet]
+        [Route("Vjezba9")]
+        public int Vj9(int x, int y) 
+        {
+            return 0;
+        }
     }
 }
