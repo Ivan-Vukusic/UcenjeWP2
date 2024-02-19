e6b3feef8bcb62351ddc761d5fb7f72e3fc929f5
Ivan Vukušić <ivukusic27@gmail.com>
2024-02-19 18:28:33 +0100

    DeratizacijaAPP - dodao sve entitete u model

---
+++ CSHARP/UcenjeWP2/DeratizacijaAPP/Models/Objekt.cs
@@ -0,0 +1,24 @@
+﻿namespace DeratizacijaAPP.Models
+{
+    /// <summary>
+    /// Ovo mi je POCO koji je mapiran na bazu
+    /// </summary>
+    public class Objekt : Entitet
+    {
+        /// <summary>
+        /// Mjesto u kojem se nalazi objekt
+        /// </summary>
+        public string? Mjesto { get; set; }
+
+        /// <summary>
+        /// Adresa objekta
+        /// </summary>
+        public string? Adresa { get; set; }
+
+        /// <summary>
+        /// Vrsta objekta
+        /// </summary>
+        public Vrsta? Vrsta { get; set; }
+
+    }
+}
---
+++ CSHARP/UcenjeWP2/DeratizacijaAPP/Models/Termin.cs
@@ -0,0 +1,33 @@
+﻿namespace DeratizacijaAPP.Models
+{
+    /// <summary>
+    /// Ovo mi je POCO koji je mapiran na bazu
+    /// </summary>
+    public class Termin : Entitet
+    {
+        /// <summary>
+        /// Datum termina
+        /// </summary>
+        public DateTime? Datum { get; set; }
+
+        /// <summary>
+        /// Djelatnik koji obavlja deratizaciju
+        /// </summary>
+        public Djelatnik? Djelatnik { get; set; }
+
+        /// <summary>
+        /// Objekt u kojem se obavlja deratizacija
+        /// </summary>
+        public Objekt? Objekt { get; set; }
+
+        /// <summary>
+        /// Otrov koji se koristi
+        /// </summary>
+        public Otrov? Otrov { get; set; }
+
+        /// <summary>
+        /// Napomena za dodatna zapažanja
+        /// </summary>
+        public string? Napomena { get; set; }
+    }
+}
