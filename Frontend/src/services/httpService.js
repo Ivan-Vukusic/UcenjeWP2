92a8bacac3877d703b45aaa3caac31ac915374fd
Ivan Vukušić <ivukusic27@gmail.com>
2024-03-15 20:16:51 +0100

    ...

--- Frontend/src/services/httpService.js
+++ Frontend/src/services/httpService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const httpService = axios.create({
     baseURL: 'https://korisnik117-001-site1.itempurl.com/api/v1',
     headers:{
-        'Content-Type' : 'application/json'
+        'Content-Type' : 'application/json; charset=utf-8'
     }
 });
