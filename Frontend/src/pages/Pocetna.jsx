d7d72c655ba992c34b4fafbb189b295ed2957c75
Ivan Vukušić <ivukusic27@gmail.com>
2024-04-04 11:10:50 +0200

    Dodana slika

--- Frontend/src/pages/Pocetna.jsx
+++ Frontend/src/pages/Pocetna.jsx
@@ -1,11 +1,17 @@
 import Container from 'react-bootstrap/Container';
+import slika1 from '../assets/images/slika1.jpg';
+
 
 export default function Pocetna(){
 
     return(
         <>
-        <Container>
-            Dobrodošli na Deratizacija aplikaciju
+        <Container className='sredina'>
+
+            <h2>Evidencija provedene deratizacije</h2>
+
+            <img src={slika1} alt="Glodavac" />             
+            
         </Container>
         </>
 
