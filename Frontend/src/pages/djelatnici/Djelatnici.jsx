7082d6de1f47962164e60a22effd8105c28a5efa
Ivan Vukušić <ivukusic27@gmail.com>
2024-04-17 13:17:12 +0200

    Tražilica i straničenje

--- Frontend/src/pages/djelatnici/Djelatnici.jsx
+++ Frontend/src/pages/djelatnici/Djelatnici.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Container, Row, Col, Card, Modal } from "react-bootstrap";
+import { Button, Container, Row, Col, Card, Modal, Form, Pagination } from "react-bootstrap";
 import DjelatnikService from "../../services/DjelatnikService";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
@@ -12,16 +12,23 @@ import useLoading from "../../hooks/useLoading";
 
 export default function Djelatnici() {
 
-    const [djelatnici, setDjelatnici] = useState();
+    const [djelatnici, setDjelatnici] = useState([]);
+    const [stranica, setStranica] = useState(1);
+    const [uvjet, setUvjet] = useState('');
     const { prikaziError } = useError();
     const { showLoading, hideLoading } = useLoading();
     const [prikaziModal, setPrikaziModal] = useState(false);
 
     async function dohvatiDjelatnike() {
         showLoading();
-        const odgovor = await DjelatnikService.get('Djelatnik');
+        const odgovor = await DjelatnikService.getStranicenje(stranica, uvjet);
         if (!odgovor.ok) {
             prikaziError(odgovor.podaci);
+            hideLoading();
+            return;
+        }
+        if (odgovor.podaci.length == 0) {
+            setStranica(stranica - 1);
             hideLoading();
             return;
         }
@@ -33,11 +40,11 @@ export default function Djelatnici() {
         showLoading();
         const odgovor = await DjelatnikService.obrisi('Djelatnik', sifra);
         hideLoading();
-        if (odgovor.ok){
+        if (odgovor.ok) {
             dohvatiDjelatnike();
             setPrikaziModal(true);
         } else {
-            prikaziError(odgovor.podaci); 
+            prikaziError(odgovor.podaci);
         }
         hideLoading();
     }
@@ -48,7 +55,7 @@ export default function Djelatnici() {
 
     useEffect(() => {
         dohvatiDjelatnike();
-    }, []);
+    }, [stranica, uvjet]);
 
     function slika(djelatnik) {
         if (djelatnik.slika != null) {
@@ -57,15 +64,61 @@ export default function Djelatnici() {
         return slika2;
     }
 
+    function promjeniUvjet(e) {
+        if (e.nativeEvent.key == "Enter") {
+            setStranica(1);
+            setUvjet(e.nativeEvent.srcElement.value);
+            setDjelatnici([]);
+        }
+    }
+
+    function povecajStranicu() {
+        setStranica(stranica + 1);
+    }
+
+    function smanjiStranicu() {
+        if (stranica == 1) {
+            return;
+        }
+        setStranica(stranica - 1);
+    }
+
     return (
 
         <Container>
-            <Link to={RoutesNames.DJELATNICI_NOVI} className="btn btn-success gumb">
-                <IoPersonAddSharp
-                    size={35}
-                /> Dodaj djelatnika
-            </Link>
 
+            <Row>
+                <Col key={1} sm={12} lg={4} md={4}>
+                    <Form.Control
+                        type='text'
+                        name='trazilica'
+                        placeholder='Dio imena i prezimena [Enter]'
+                        maxLength={255}
+                        defaultValue=''
+                        onKeyUp={promjeniUvjet}
+                    />
+                </Col>
+                <Col key={2} sm={12} lg={4} md={4}>
+                    {djelatnici && djelatnici.length > 0 && (
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <Pagination size="lg">
+                                <Pagination.Prev onClick={smanjiStranicu} />
+                                <Pagination.Item disabled>{stranica}</Pagination.Item>
+                                <Pagination.Next
+                                    onClick={povecajStranicu}
+                                />
+                            </Pagination>
+                        </div>
+                    )}
+                </Col>
+                <Col key={3} sm={12} lg={4} md={4}>
+                    <Link to={RoutesNames.DJELATNICI_NOVI} className="btn btn-success gumb">
+                        <IoPersonAddSharp
+                            size={35}
+                        /> Dodaj djelatnika
+                    </Link>
+                </Col>
+            </Row>
 
             <Row>
 
@@ -93,6 +146,20 @@ export default function Djelatnici() {
                 ))
                 }
             </Row>
+
+            <hr />
+              {djelatnici && djelatnici.length > 0 && (
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Pagination size="lg">
+                    <Pagination.Prev onClick={smanjiStranicu} />
+                    <Pagination.Item disabled>{stranica}</Pagination.Item> 
+                    <Pagination.Next
+                        onClick={povecajStranicu}
+                    />
+                    </Pagination>
+                </div>
+                )}
+
             <Modal show={prikaziModal} onHide={zatvoriModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Uspješno</Modal.Title>
@@ -101,7 +168,7 @@ export default function Djelatnici() {
                 <Modal.Footer>
                     <Button variant='secondary' onClick={zatvoriModal}>Zatvori</Button>
                 </Modal.Footer>
-            </Modal>            
+            </Modal>
         </Container>
 
     );
--- Frontend/src/services/DjelatnikService.js
+++ Frontend/src/services/DjelatnikService.js
@@ -1,14 +1,25 @@
-import { httpService, obradiGresku, obradiUspjeh, get,obrisi,dodaj,getBySifra,promjeni } from "./httpService";
+import { httpService, obradiGresku, obradiUspjeh, get, obrisi, dodaj, getBySifra, promjeni } from "./httpService";
+
+async function traziDjelatnik(naziv, uvjet) {
+  return await httpService.get('/' + naziv + '/trazi/' + uvjet).then((res) => { return obradiUspjeh(res); }).catch((e) => { return obradiGresku(e); });
+}
 
 async function postaviSliku(sifra, slika) {
-    return await httpService.put('/Djelatnik/postaviSliku/' + sifra, slika).then((res)=>{return obradiUspjeh(res);}).catch((e)=>{ return obradiGresku(e);});
-  }
+  return await httpService.put('/Djelatnik/postaviSliku/' + sifra, slika).then((res) => { return obradiUspjeh(res); }).catch((e) => { return obradiGresku(e); });
+}
 
-export default{
-    get,
-    obrisi,
-    dodaj,
-    promjeni,
-    getBySifra,
-    postaviSliku    
+async function getStranicenje(stranica, uvjet) {
+  return await httpService.get('/Djelatnik/traziStranicenje/' + stranica + '?uvjet=' + uvjet)
+    .then((res) => { return obradiUspjeh(res); }).catch((e) => { return obradiGresku(e); });
+}
+
+export default {
+  get,
+  obrisi,
+  dodaj,
+  promjeni,
+  getBySifra,
+  traziDjelatnik,
+  postaviSliku,
+  getStranicenje
 };
